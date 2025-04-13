51458d9f74d53f97cb574903398bb528892baebe
Prerit Oberai <prerit.oberai@gmail.com>
2025-04-13 12:06:59 -0400

    Initial commit from Create Next App

---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '65ch',
+            color: 'inherit',
+            a: {
+              color: 'inherit',
+              opacity: 0.75,
+              '&:hover': {
+                opacity: 1,
+                color: '#3b82f6',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
+export default config 
