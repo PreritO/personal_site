51fc3907b182a787eb61b27884cfb723b87df67b
Prerit Oberai <prerit.oberai@gmail.com>
2025-08-13 17:12:02 -0400

    Changed description on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,19 @@ export default function Home() {
       <div className="site-header">
         <h1 className="site-title">Hey, I'm Prerit!</h1>
         <p className="site-description">
-        I currently work at Microsoft on the Office Security Team as a SWE. Before that, I was on the Excel team for 3 years and prior to that, I studied EECS at CU Boulder for both my undergrad and masters. My current interests lie at the intersection of consumer tech and security.
+        I live in New York and I currently work at Microsoft on the Office Security Team as a SWE. Before that, I was on the Excel team for 3 years (in Seattle) and prior to that, I studied EECS at CU Boulder for both my undergrad and masters. I'm excited about the potential of technology to improve people's lives and enjoy building high quality software that can scale quickly.
         </p>
         <p className="site-description">
-          I also love to learn how things work generally (currently reading about High-Frequency Trading) and am fascinated by how businesses scale to have millions of users. Outside of my nerdy musings, I like playing basketball, running and generally reading biographies. If you have any recs, please send them my way. My vision for a successful life entails doing meaningful work that brings you joy, surrounding yourself with people who care for you while developing relationships that allow you to grow and positivily having an impact on the world that will leave it better for future generations.
+          I love to learn how things work generally and am fascinated by how businesses scale to millions. Outside of my nerdy musings, I like playing basketball, running and generally reading biographies. If you have any recs, please send them my way. My vision for a successful life entails doing meaningful work that brings you joy, surrounding yourself with people who care for you while developing relationships that allow you to grow. 
         </p>
+         <p className="site-description">
+          I'm interested in connecting with people who also enjoy: <br />
+          - Software development and engineering best practices <br />
+          - Entrepreneurship<br />
+          - Fitness & Sports<br />
+          - Reading and sharing book recommendations <br />
+          - Tools for Thoughts and creating a 2nd Brain <br />
+         </p>
         <div className="social-links">
           <a
             href="https://github.com/preritoberai"
