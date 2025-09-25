089bc8c847a2cbedc3ae43fe398543b32e2ca726
Prerit Oberai <prerit.oberai@gmail.com>
2025-09-25 10:28:09 -0400

    lil homepage updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
       <div className="site-header">
         <h1 className="site-title">Hey, I'm Prerit!</h1>
         <p className="site-description">
-        I live in New York and I currently work at Microsoft on the Office Security Team as a SWE. Before that, I was on the Excel team for 3 years (in Seattle) and prior to that, I studied EECS at CU Boulder for both my undergrad and masters. I'm excited about the potential of technology to improve people's lives and enjoy building high quality software that can scale quickly.
+        I live in New York and currently work as a Founding Engineer at a very early stage startup focusing on building Patient Navigation Services for the elderly. Prior to that, I was a SWE at Microsoft on the Office Security and Excel teams for 4 years and before that, I studied EECS at CU Boulder for both my undergrad and graduate degrees. I'm excited about leveraging technology to improve people's everyday lives and enjoy building high quality software that can scale quickly.
         </p>
         <p className="site-description">
-          I love to learn how things work generally and am fascinated by how businesses scale to millions. Outside of my nerdy musings, I like playing basketball, running and generally reading biographies. If you have any recs, please send them my way. My vision for a successful life entails doing meaningful work that brings me joy, surrounding myself with people who care for me while developing relationships that allow me to grow.  
+          I love to learn how things work and am fascinated by how businesses scale from zero to millions. Outside of my nerdy musings, I like playing basketball, running and reading biographies. If you have any recs, please send them my way. My vision for a successful life entails doing meaningful work that brings me joy, surrounding myself with people who care for me while developing relationships that allow me to grow.  
         </p>
          <p className="site-description">
           I'm interested in connecting with people who also enjoy: <br />
